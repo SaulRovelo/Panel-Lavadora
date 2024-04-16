56c0371568ae2e7b70282cfef627ade21e84ea89
Saul Rovelo <sr031437@gmail.com>
2024-04-15 21:46:36 -0600

    Se añadio el directorio boton temperatura con su respectivo programa y cmake al igual se modifico el cmake externo como interno

---
+++ botontemperatura/botontemp.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+
+#define LED_PIN_1 0
+#define LED_PIN_2 1
+#define LED_PIN_3 2
+#define BUTTON_PIN 3
+
+int main(){
+    // Función inicialización 
+    stdio_init_all();
+
+    // Inicializamos los LEDs
+    gpio_init(LED_PIN_1);
+    gpio_set_dir(LED_PIN_1, GPIO_OUT);
+    gpio_init(LED_PIN_2);
+    gpio_set_dir(LED_PIN_2, GPIO_OUT);
+    gpio_init(LED_PIN_3);
+    gpio_set_dir(LED_PIN_3, GPIO_OUT);
+
+    // Inicializamos el botón
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+
+    // Variable para controlar el estado de los LEDs
+    int contador = 1;
+
+    
+    while (true){
+        if (gpio_get(BUTTON_PIN) == 0) // Verificar el estado del botón (0 o 1)
+        {
+            // Enciende el siguiente LED y apaga el anterior
+            if (contador == 1) 
+            {
+                gpio_put(LED_PIN_1, 1); // Encender LED 1
+                gpio_put(LED_PIN_2, 0); // Apagar LED 2
+                gpio_put(LED_PIN_3, 0); // Apagar LED 3
+                printf("LED 1 encendido\n");
+                contador = 2;
+            } 
+            else if (contador == 2) 
+            {
+                gpio_put(LED_PIN_1, 0); // Apagar LED 1
+                gpio_put(LED_PIN_2, 1); // Encender LED 2
+                gpio_put(LED_PIN_3, 0); // Apagar LED 3
+                printf("LED 2 encendido\n");
+                contador = 3;
+            }
+            else if (contador == 3) 
+            {
+                gpio_put(LED_PIN_1, 0); // Apagar LED 1
+                gpio_put(LED_PIN_2, 0); // Apagar LED 2
+                gpio_put(LED_PIN_3, 1); // Encender LED 3
+                printf("LED 3 encendido\n");
+                contador = 4;
+            }
+            else if (contador == 4) 
+            {
+                gpio_put(LED_PIN_1, 0); // Apagar LED 1
+                gpio_put(LED_PIN_2, 0); // Apagar LED 2
+                gpio_put(LED_PIN_3, 0); // Apagar LED 3
+                printf("Todos los LEDs apagados\n");
+                contador = 1;
+            }
+            while(gpio_get(BUTTON_PIN) == 0); // Esperar hasta que el botón se suelte
+        }
+    }
+    
+    return 0;
+}
---
+++ common/sdk_inlines.c
@@ -0,0 +1,27 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include "pico/stdlib.h"
+//#include "hardware/gpio.h"
+void gpio_set_dir_wrapper(unsigned gpio, bool out)
+{
+    gpio_set_dir(gpio, out);
+}
+
+void gpio_pull_up_wrapper(unsigned gpio)
+{
+    gpio_pull_up (gpio);
+
+}
+
+bool gpio_get_wrapper(unsigned gpio){
+return gpio_get (gpio);
+}
+
+
+void gpio_set_mask_wrapper(long mask){
+gpio_set_mask(mask);
+}
+
+void gpio_clr_mask_wrapper(long mask){
+gpio_clr_mask(mask);
+}
