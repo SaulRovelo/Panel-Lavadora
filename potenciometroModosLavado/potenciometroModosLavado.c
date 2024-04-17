280714233dbb7164c55c7679afc1b27b7374a39d
Kevin Dulche Laptop GMR Ubuntu <kevdulche@gmail.com>
2024-04-17 13:44:57 -0600

    Elimino comentarios que no se usan

--- potenciometroModosLavado/potenciometroModosLavado.c
+++ potenciometroModosLavado/potenciometroModosLavado.c
@@ -6,31 +6,25 @@
 #define FIRST_GPIO 2
 #define SLEEP 50
 
-int main() {
+int bits[10] = {
+    0x01,  // 1
+    0x02,  // 2
+    0x04,  // 3
+    0x08,  // 4
+    0x10,  // 5
+    0x20,  // 6
+};
 
-    int bits[10] = {
-        0x01,  // 1
-        0x02,  // 2
-        0x04,  // 3
-        0x08,  // 4
-        0x10,  // 5
-        0x20,  // 6
-    };
+int main() {
 
     stdio_init_all();
     adc_init();
     adc_gpio_init(POT_PIN);
     adc_select_input(0);
 
-    // We could use gpio_set_dir_out_masked() here
     for (int gpio = FIRST_GPIO; gpio < FIRST_GPIO + 6; gpio++) {
-        printf("GPIO: %d\n", gpio);
         gpio_init(gpio);
         gpio_set_dir(gpio, GPIO_OUT);
-        // Our bitmap above has a bit set where we need an LED on, BUT, we are pulling low to light
-        // so invert our output
-        // gpio_set_outover(gpio, GPIO_OVERRIDE_INVERT); //Así es para anodo común
-        // sleep_ms(SLEEP);
     }
 
     int value = 0;
@@ -55,20 +49,9 @@ int main() {
         // We are starting with GPIO 2, our bitmap starts at bit 0 so shift to start at 2.
         int32_t mask = bits[value] << FIRST_GPIO;
 
-        // Set all our GPIOs in one go!
-        // If something else is using GPIO, we might want to use gpio_put_masked()
         gpio_set_mask(mask);
         sleep_ms(SLEEP);
         gpio_clr_mask(mask);
-
-        /*
-        printf("ADC Value: %d\n", adc_value);
-        // Convertir la lectura del ADC a voltaje (asumiendo una referencia de 3.3V)
-        float voltage = adc_value * (3.3 / 65535.0);
-        // Imprimir el valor del voltaje en el terminal
-        printf("Voltage: %fV\n", voltage);
-        sleep_ms(1000);
-        */
     }
     return 0;
 }
