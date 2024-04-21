2a290dbb5a510a09775e8492c9496aff4d3eb608
yayo <yayomar211@gmail.com>
2024-04-20 19:26:49 -0600

    Eliminación de main

--- botonInicioPausa/botonIP.c
+++ botonInicioPausa/botonIP.c
@@ -1,6 +1,6 @@
 #include "botonIP.h"
 // Este array convierte un número del 0 al 9 en un patrón de bits para enviar a los GPIOs
-int bits[10] = {
+static int bits[10] = {
     0x3f, // 0
     0x06, // 1
     0x5b, // 2
@@ -22,46 +22,7 @@ void initGpioButton(){
 }
 
 void displayLoop(){
-    
+
 }
 
-int main() {
-    stdio_init_all(); // Inicializar las funciones de entrada/salida estándar del Pico
 
-    initGpioSegmentDisplay();
-    initGpioButtonSegmentDisplay();
-    int val = 9;  // Valor inicial para mostrar en el display
-    bool paused = false;  // Indicador de si el display está en pausa
-    uint64_t ultimaPulsacion = 0;  // Registrar el último tiempo de pulsación para el debounce
-    const uint64_t debounce_time = 100;  // Tiempo de debounce en milisegundos
-
-    while (true) {
-        // Comprobar si el botón es presionado con debouncing
-        if (!gpio_get(BUTTON) && to_ms_since_boot(get_absolute_time()) - ultimaPulsacion > debounce_time) {
-            paused = !paused;  // Alternar el estado de pausa
-            ultimaPulsacion = to_ms_since_boot(get_absolute_time());  // Actualizar el último tiempo de pulsación
-        }
-
-        // Mostrar el valor actual en el display de 7 segmentos solo si no está en pausa
-        if (!paused) {
-            if (val >= 0){
-                int32_t mask = bits[val] << S1;  // Calcular la máscara de bits para los pines del display
-                gpio_set_mask(mask);  // Establecer los pines del GPIO según la máscara de bits
-                sleep_ms(500);  // Mostrar el número durante 500 milisegundos
-                gpio_clr_mask(mask);  // Apagar los pines del GPIO
-                val--;
-            }  else {
-                printf("Ropa Limpia :))");
-                break;
-            }
-            
-        } else {
-            // Cuando está en pausa, mostrar continuamente el valor actual sin cambiar
-            int32_t mask = bits[val] << S1;
-            gpio_set_mask(mask);  // Mantener los pines del GPIO establecidos continuamente
-            sleep_ms(500);  // Apagar durante 500 milisegundos
-            gpio_clr_mask(mask);  // Apagar los pines del GPIO
-            sleep_ms(500);
-        }
-    }
-}
