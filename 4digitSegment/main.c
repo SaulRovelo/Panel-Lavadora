4349813aeb2a51f9ffafb53c37f7b7bcbce9f6ec
yayo <yayomar211@gmail.com>
2024-04-20 22:14:23 -0600

    Declaración de bits

--- 4digitSegment/main.c
+++ 4digitSegment/main.c
@@ -4,7 +4,21 @@
 
 #define firstGpio 6
 
+int nums[12] = {
+    0x3f, // 0
+    0x06, // 1
+    0x5b, // 2
+    0x4f, // 4
+    0x66, // 5
+    0x7d, // 6
+    0x07, // 7
+    0x7f, // 8
+    0x67, // 9
+    0x76, // H
+    0x77 // A
+};
+
 int main(){
     stdio_init_all();
-    
+
 }
