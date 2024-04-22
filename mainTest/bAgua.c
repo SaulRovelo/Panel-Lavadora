74c3dae223b65c45aef5b2962a71d15928e8c217
Samuel Perez <samuel.perez@cua.uam.mx>
2024-04-22 17:19:12 -0600

    Subiendo Imagenes de los esquemas de botones

--- mainTest/bAgua.c
+++ mainTest/bAgua.c
@@ -14,11 +14,20 @@
 static int contador = 0;
 
 void inicializar_leds_agua() {
+    // Función inicialización 
+    //stdio_init_all();
+
     // Inicializamos los LEDs
-    for (int i = 0; i < 5; i++) {
-        gpio_init(i);        // Inicializamos el pin
-        gpio_set_dir(i, GPIO_OUT); // Configuramos como salida
-    }
+    gpio_init(LED_PIN_1);
+    gpio_set_dir(LED_PIN_1, GPIO_OUT);
+    gpio_init(LED_PIN_2);
+    gpio_set_dir(LED_PIN_2, GPIO_OUT);
+    gpio_init(LED_PIN_3);
+    gpio_set_dir(LED_PIN_3, GPIO_OUT);
+    gpio_init(LED_PIN_4);
+    gpio_set_dir(LED_PIN_4, GPIO_OUT);
+    gpio_init(LED_PIN_5);
+    gpio_set_dir(LED_PIN_5, GPIO_OUT);
 
     // Inicializamos el botón
     gpio_init(BUTTON_PIN_2);
