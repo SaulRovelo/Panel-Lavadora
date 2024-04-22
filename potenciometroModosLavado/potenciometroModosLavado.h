6098e0eacdaec311c19aff14d38e7dd427428bec
Kevin-Dulche <kevdulche@gmail.com>
2024-04-21 19:07:47 -0600

    Actualizar CMake y creo archivo de variables

--- potenciometroModosLavado/potenciometroModosLavado.h
+++ potenciometroModosLavado/potenciometroModosLavado.h
@@ -2,23 +2,7 @@
 #include "pico/stdlib.h"
 #include "hardware/adc.h"
 
-#define POT_PIN 26
-#define FIRST_GPIO 2
-
-static int bits[10] = {
-    0x01,  // 1
-    0x02,  // 2
-    0x04,  // 3
-    0x08,  // 4
-    0x10,  // 5
-    0x20,  // 6
-};
-
-static int value = 0;
-
-static int32_t mask;
-
-static uint16_t adc_value;
+#include "variables.h"
 
 /**
  * @brief Inicializa los pines de los leds y el potenciometro.
