915ecfaeb5ffb5cc5503f796989c1a3b6889000a
Saul Rovelo <sr031437@gmail.com>
2024-04-22 21:39:07 -0600

    Agregamos variables y condicional para verificar cuantas vees se presiona el boton

--- include/Variables.h
+++ include/Variables.h
@@ -1,4 +1,9 @@
+#include <stdio.h>
 #define LED1_PIN 14
 #define LED2_PIN 15
 #define LED3_PIN 16
 #define BUTTON_PIN 17
+uint32_t tiempo_anterior = 0;
+bool boton_presionado = false;
+uint32_t tiempo_presionado = 0;
+int pulsaciones = 0;
--- include/botontareas.h
+++ include/botontareas.h
@@ -23,14 +23,15 @@ bool condicion() {
     // Verificar el estado del botón (0 o 1)
     return gpio_get(BUTTON_PIN) == 0;
 }
-void handleLEDs() {
+void ciclos() {
+    
     uint32_t tiempo_actual = time_us_32(); // Obtener tiempo actual en microsegundos
 
     if (pulsaciones > 3) {
         pulsaciones = 0; // Reiniciar contador de pulsaciones si es mayor que 3
     }
 
-    if (isButtonPressed()) {
+    if (condicion()) {
         // Verificamos si han pasado menos de 500ms desde la última pulsación
         if (tiempo_actual - tiempo_anterior < 500000) {
             pulsaciones++; // Incrementar contador de pulsaciones
@@ -38,3 +39,19 @@ void handleLEDs() {
             pulsaciones = 1; // Reiniciar contador de pulsaciones
         }
     }
+    
+     tiempo_anterior = tiempo_actual; // Actualizar tiempo anterior
+
+        if (!boton_presionado) {
+            tiempo_presionado = tiempo_actual; // Actualizar tiempo de inicio de presión del botón
+            boton_presionado = true; // Indicar que el botón está siendo presionado
+        } else {
+            if (tiempo_actual - tiempo_presionado >= 1000) { // Si el botón se mantiene presionado por 2 segundos
+                gpio_put(LED1_PIN, 0); // Apagar LED 1
+                gpio_put(LED2_PIN, 0); // Apagar LED 2
+                gpio_put(LED3_PIN, 0); // Apagar LED 3
+                pulsaciones = 0; // Reiniciar contador de pulsaciones
+            }
+        }
+
+}
