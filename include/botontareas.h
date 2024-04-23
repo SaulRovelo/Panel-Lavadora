414e7f9c04ec2518dd07fefc4687b77954970ee8
Saul Rovelo <sr031437@gmail.com>
2024-04-22 21:35:42 -0600

    Agregamos ciclos y una condicional para las pulsaciones

--- include/botontareas.h
+++ include/botontareas.h
@@ -19,7 +19,22 @@ void inicializar() {
     gpio_pull_up(BUTTON_PIN);
 }
 
-bool isButtonPressed() {
+bool condicion() {
     // Verificar el estado del botón (0 o 1)
     return gpio_get(BUTTON_PIN) == 0;
 }
+void handleLEDs() {
+    uint32_t tiempo_actual = time_us_32(); // Obtener tiempo actual en microsegundos
+
+    if (pulsaciones > 3) {
+        pulsaciones = 0; // Reiniciar contador de pulsaciones si es mayor que 3
+    }
+
+    if (isButtonPressed()) {
+        // Verificamos si han pasado menos de 500ms desde la última pulsación
+        if (tiempo_actual - tiempo_anterior < 500000) {
+            pulsaciones++; // Incrementar contador de pulsaciones
+        } else {
+            pulsaciones = 1; // Reiniciar contador de pulsaciones
+        }
+    }
