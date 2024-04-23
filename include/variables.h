eb278bdee6ec5f51093eb586720c4f04a4186250
Samuel Perez <samuel.perez@cua.uam.mx>
2024-04-22 20:56:50 -0600

    Modificacion de bAgua.h

--- include/variables.h
+++ include/variables.h
@@ -1,5 +1,13 @@
 #define LED_PIN_0 0        // Define el pin del LED
 #define BUTTON_PIN_1 1     // Define el pin del botón
 
+#define LED_PIN_2 2
+#define LED_PIN_3 3
+#define LED_PIN_4 4
+#define LED_PIN_5 5
+#define LED_PIN_6 6
+#define BUTTON_PIN_7 7
+
 static bool led_encendido = false;  // Variable para controlar el estado del LED
 
+static int contador = 0; // Variable para controlar el estado de los LEDs
