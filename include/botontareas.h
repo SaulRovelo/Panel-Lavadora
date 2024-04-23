e8be3290633ce93f398a9f916229f5a8f440b1c6
Saul Rovelo <sr031437@gmail.com>
2024-04-22 21:28:44 -0600

    Se agrega funcion inicializar

---
+++ include/botontareas.h
@@ -0,0 +1,20 @@
+
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include "Variables.h"
+
+void inicializar() {
+
+    // Inicialización de los LED
+    gpio_init(LED1_PIN);
+    gpio_set_dir(LED1_PIN, GPIO_OUT);
+    gpio_init(LED2_PIN);
+    gpio_set_dir(LED2_PIN, GPIO_OUT);
+    gpio_init(LED3_PIN);
+    gpio_set_dir(LED3_PIN, GPIO_OUT);
+
+    // Inicialización del botón
+    gpio_init(BUTTON_PIN);
+    gpio_set_dir(BUTTON_PIN, GPIO_IN);
+    gpio_pull_up(BUTTON_PIN);
+}
