4c89c6d73d675ee1c2670241f7f8dc4236781c14
Saul Rovelo <sr031437@gmail.com>
2024-04-22 21:44:34 -0600

    agregamos cmake y librerias al main

--- include/botontareas.h
+++ include/botontareas.h
@@ -1,6 +1,8 @@
 
 #include <stdio.h>
+
 #include <pico/stdlib.h>
+
 #include "Variables.h"
 
 void inicializar() {
---
+++ main2/main.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+
+#include "botontareas.h"
