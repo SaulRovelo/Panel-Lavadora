6905861e20c6e6bbc75c99fdbc518f60fd94dd18
Saul Rovelo <sr031437@gmail.com>
2024-04-23 22:07:36 -0600

    agregamos logica main

---
+++ Botontareas/botontareas.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <pico/stdlib.h>
+#include "botontareas.h"
+#include "botontemperatura.h"
+
+int main(){
+    stdio_init_all();
+    inicializar_leds_tareas();
+
+while (true)
+{
+       control_leds_tareas();
+       sleep_ms(200);
+       
+}
+ }
