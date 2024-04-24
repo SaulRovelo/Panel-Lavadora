6f92164ccd6ef799be55c9a04c8e51afd6e8fca2
Samuel Perez <samuel.perez@cua.uam.mx>
2024-04-23 22:03:36 -0600

    Modificacion de carpeta boton nivel agua y encendido para el main

---
+++ botonAgua/variables.h
@@ -0,0 +1,20 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+#ifndef VARIABLES_H
+#define VARIABLES_H
+
+// Variables boton de nivel de agua
+
+#define LED_PIN_2 2
+#define LED_PIN_3 3
+#define LED_PIN_4 4
+#define LED_PIN_5 5
+#define LED_PIN_6 6
+#define BUTTON_PIN_7 7
+
+static int contador = 0; 
+
+bool lavadora_encendida = false;
+
+#endif // VARIABLES_H
--- botonEncendido/boton_encendido.h
+++ botonEncendido/boton_encendido.h
@@ -1,14 +1,7 @@
-#include "boton_encendido.h"  // Incluye el archivo de encabezado para el botón de encendido
 #include <stdio.h>
-#include <pico/stdlib.h>
 #include "pico/stdlib.h"
+#include "variables.h"
 
-#define LED_PIN_0 0        // Define el pin del LED
-#define BUTTON_PIN_1 1     // Define el pin del botón
-
-//static bool led_encendido = false;  // Variable para controlar el estado del LED
-
-// Función para inicializar el botón de encendido y el LED
 void inicializar_boton_encendido() {
     //stdio_init_all();   // Inicializa las funciones de E/S estándar
     gpio_init(LED_PIN_0);   // Inicializa el pin del LED
@@ -28,11 +21,23 @@ void control_leds_encendido() {
     if (!led_encendido) {   // Si el LED está apagado
         gpio_put(LED_PIN_0, 1);   // Enciende el LED
         led_encendido = true;   // Actualiza el estado del LED
-        printf("Lavadora encendida\n");   // Imprime un mensaje indicando que la lavadora está encendida
+        //printf("Lavadora encendida\n");   // Imprime un mensaje indicando que la lavadora está encendida
     } else {   // Si el LED está encendido
         gpio_put(LED_PIN_0, 0);   // Apaga el LED
         led_encendido = false;   // Actualiza el estado del LED
-        printf("Lavadora apagada\n");   // Imprime un mensaje indicando que la lavadora está apagada
+        //printf("Lavadora apagada\n");   // Imprime un mensaje indicando que la lavadora está apagada
     }
-    //sleep_ms(200);   // Espera 200 ms
+    //sleep_ms(200);   
 }
+
+// Implementación de la función para apagar todos los LEDs y reiniciar el contador
+// void apagar_y_reiniciar_leds() {
+//     Apaga todas las salidas GPIO
+//     for (int pin = 0; pin < TOTAL_GPIO_PINS; pin++) {
+//         gpio_put(pin, 0);
+//     }
+//     Reinicia el contador a su estado inicial
+//     contador = 0;
+// }
+
+
