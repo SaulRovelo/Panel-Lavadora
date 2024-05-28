308082592d8f99e72c94d5d7f0ae2e61fa9b5238
Eduardo Martínez Varinas <yayomar211@gmail.com>
2024-05-28 11:40:25 -0600

    Agregacion de meta a html

--- botonInicioPausaWeb/boton.py
+++ botonInicioPausaWeb/boton.py
@@ -12,6 +12,8 @@ def web_page():
     html = """<html>
     <head>
     <title>Boton de inicio y pausa</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="data:,">
     </head>
     <body>
     <h1>Boton de inicio y pausa</h1>
