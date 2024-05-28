bdaacccfce9c68d98858d59b952c476ad6764b50
Samuel Perez <samuel.perez@cua.uam.mx>
2024-05-28 00:49:09 -0600

    Añadiendo boton cambiar velocidad

--- motor_test.py
+++ motor_test.py
@@ -1,32 +1,39 @@
 from machine import Pin, PWM
-import time
+import utime
 
 # Pines para el control del motor
 motor1 = Pin(19, Pin.OUT)
 pwm = PWM(Pin(18))
+
+# Pin para el botón
+boton = Pin(16, Pin.IN, Pin.PULL_DOWN)
 
 # Inicializar el pin PWM y el motor apagado
 motor1.value(0)
 pwm.freq(1000)
 pwm.duty_u16(0)
 
-continuar = True
+# Velocidades definidas
+velocidades = [0, 21845, 43690, 65535]  # 0%, 33%, 66%, 100%
+indice_velocidad = 0
 
-while continuar:
-    try:
-        vel = input('Digite velocidad de 0 a 100 (o "z" para salir): ')
-        if vel.lower() == 'z':
-            continuar = False
-            pwm.deinit()
-        else:
-            velocidad = int(vel)
-            if 0 <= velocidad <= 100:
-                # Convertir la velocidad de 0-100 a 0-65535
-                duty = int(velocidad * 655.35)
-                pwm.duty_u16(duty)
-            else:
-                print('Por favor, digite un valor entre 0 y 100.')
-    except ValueError:
-        print('Entrada no válida, por favor digite un número o "z" para salir.')
+def cambiar_velocidad(pin):
+    print('Boton')
+    global indice_velocidad
+    indice_velocidad = (indice_velocidad + 1) % len(velocidades)
+    pwm.duty_u16(velocidades[indice_velocidad])
+    print(f'Velocidad cambiada a: {int((velocidades[indice_velocidad] / 65535) * 100)}%')
 
-print('Fin de programa')
+# Configurar la interrupción del botón
+boton.irq(trigger=Pin.IRQ_FALLING, handler=cambiar_velocidad)
+
+print('Presiona el botón para cambiar la velocidad del motor.')
+
+try:
+    while True:
+        # Mantén el programa corriendo para que pueda responder a la interrupción
+        utime.sleep(1)
+except KeyboardInterrupt:
+    # Limpiar los recursos al salir del programa
+    pwm.deinit()
+    print('Fin de programa')
