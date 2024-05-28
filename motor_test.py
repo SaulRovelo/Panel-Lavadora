715212f96ce52b2eb1d88fd0c8209a5edc8a493c
Samuel Perez <samuel.perez@cua.uam.mx>
2024-05-28 00:50:39 -0600

    Añadiendo led's para velocidad

--- motor_test.py
+++ motor_test.py
@@ -5,6 +5,11 @@ import utime
 motor1 = Pin(19, Pin.OUT)
 pwm = PWM(Pin(18))
 
+# Pines para los LEDs
+led1 = Pin(13, Pin.OUT)
+led2 = Pin(12, Pin.OUT)
+led3 = Pin(11, Pin.OUT)
+
 # Pin para el botón
 boton = Pin(16, Pin.IN, Pin.PULL_DOWN)
 
@@ -18,11 +23,27 @@ velocidades = [0, 21845, 43690, 65535]  # 0%, 33%, 66%, 100%
 indice_velocidad = 0
 
 def cambiar_velocidad(pin):
-    print('Boton')
     global indice_velocidad
     indice_velocidad = (indice_velocidad + 1) % len(velocidades)
     pwm.duty_u16(velocidades[indice_velocidad])
+    actualizar_leds()
     print(f'Velocidad cambiada a: {int((velocidades[indice_velocidad] / 65535) * 100)}%')
+
+def actualizar_leds():
+    # Apagar todos los LEDs
+    led1.value(0)
+    led2.value(0)
+    led3.value(0)
+    # Encender el LED correspondiente a la velocidad actual
+    if indice_velocidad == 1:
+        print('led 1')
+        led1.value(1)
+    elif indice_velocidad == 2:
+        print('led 2')
+        led2.value(1)
+    elif indice_velocidad == 3:
+        print('led 3')
+        led3.value(1)
 
 # Configurar la interrupción del botón
 boton.irq(trigger=Pin.IRQ_FALLING, handler=cambiar_velocidad)
