191831f60bce8dfabd38a3c8cf97f47435df13c6
Saul Rovelo <sr031437@gmail.com>
2024-05-28 02:10:17 -0600

    iintentamos imprimird distancia

--- main.py
+++ main.py
@@ -3,6 +3,10 @@ from SensorUltrasonico import SensorUltrasonico
 import utime
 
 def Sensor(): 
+    Inicializacion()
+    while True:
+        distancia=SensorUltrasonico.medir(trigger,echo)
+        print(f"Distancia: {distancia:.2f} cm")
 
 
 if __name__ == "__main__":
