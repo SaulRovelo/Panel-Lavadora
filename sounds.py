0564f4fd4ee43de41e97e96ad29d11f0bebf3f5a
Eduardo Martínez Varinas <yayomar211@gmail.com>
2024-05-29 14:18:16 -0600

    Se agregan documentación

--- sounds.py
+++ sounds.py
@@ -3,6 +3,13 @@ from utime import sleep_ms
 
 
 def play_tone( frequency, duration , buzz: PWM):
+    """Play a tone with the given frequency and duration.
+
+    Args:
+        frequency (int): Frequency of the tone
+        duration (int): Duration of the tone
+        buzz (PWM): Object of the class PWM 
+    """
     buzz.freq(frequency)
     buzz.duty_u16(35000)
     sleep_ms(duration)
